7dd5c8dfe4aa0b9645fa7727cc5d44b2ca09c97f
Jasmin <jaxxhands2112@gmail.com>
2025-08-04 16:16:10 -0400

    Updating...

--- docs/conf.py
+++ docs/conf.py
@@ -35,4 +35,8 @@ html_theme = 'insegel'
 html_static_path = ['_static']
 
 def setup(app):
-    app.add_css_file("css/custom.css")
+    app.add_css_file('css/custom.css')
+
+html_theme_options = {
+    'navigation_depth': 2,
+}
